be3e49169b0ddec6a6170928916034879f9d5e20
Nojus Stankevičius <Nojus869@gmail.com>
2024-02-11 22:04:48 +0200

    Aprasiau struktura studento duomenims saugoti

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+struct Student {
+    string firstName, lastName;
+    vector<int> homeworkResults;
+    int examResults;
+};
+
+int main() {
+    return 0;
+}
