db9a42709028912bdc501474cbcb2e91b1354042
Nojus Stankevičius <Nojus869@gmail.com>
2024-02-11 22:19:46 +0200

    Sukuriau funkcija duomenu ivedimui

--- main.cpp
+++ main.cpp
@@ -9,6 +9,28 @@ struct Student {
     int examResults;
 };
 
+void input();
+
 int main() {
     return 0;
+}
+
+void input(){
+    Student data;
+    cout << "Iveskite studento varda: ";
+    cin >> data.firstName;
+    cout << "Iveskite studento pavarde: ";
+    cin >> data.lastName;
+    cout << "Iveskite studento namu darbu kieki (n): ";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Iveskite " << i + 1 << "-aji namu darba: ";
+        int grade;
+        cin >> grade;
+        data.homeworkResults.push_back(grade);
+    }
+    cout << "Iveskite studento egzamino rezultata: ";
+    cin >> data.examResults;
+    
 }
