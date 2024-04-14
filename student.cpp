c4b8993bc05d3c4f543a2930f4ef0b3402813e0b
Nojus Stankevičius <148903463+nojusta@users.noreply.github.com>
2024-04-14 16:41:38 +0300

    komentarai

--- student.cpp
+++ student.cpp
@@ -1,17 +1,18 @@
 #include "student.h"
 
-Student::Student() : firstName(""), lastName(""), examResults(0) {}
+Student::Student() : firstName(""), lastName(""), examResults(0) {} // default konstruktorius
 
+// konstruktorius su parametrais
 Student::Student(const std::string& firstName, const std::string& lastName, int examResults, const std::vector<int>& homeworkResults)
     : firstName(firstName), lastName(lastName), examResults(examResults), homeworkResults(homeworkResults) {}
 
-double Student::calculateMedian() const {
-    if (homeworkResults.empty()) {
+double Student::calculateMedian() const { 
+    if (homeworkResults.empty()) { 
         return 0;
     }
-    std::vector<int> scores = homeworkResults;
-    sort(scores.begin(), scores.end());
-    int size = scores.size();
+    std::vector<int> scores = homeworkResults; 
+    sort(scores.begin(), scores.end()); 
+    int size = scores.size(); 
     if (size % 2 == 0) { 
         return (scores[size / 2 - 1] + scores[size / 2]) / 2.0; 
     } else {
@@ -30,7 +31,7 @@ double Student::calculateAverage() const {
     return sum / homeworkResults.size();
 }
 
-double Student::calculateFinalGrade(bool median) const {
+double Student::calculateFinalGrade(bool median) const { 
     double homeworkGrade = median ? calculateMedian() : calculateAverage();
     return 0.4 * homeworkGrade + 0.6 * examResults;
 }
--- student.h
+++ student.h
@@ -9,7 +9,7 @@
 #include <deque>
 #include <list>
 
-class Student {
+class Student { // klase Student
 private:
     std::string firstName, lastName;
     std::vector<int> homeworkResults;
@@ -17,26 +17,29 @@ private:
 
 public:
 
-    Student(); 
-    
+    Student(); // konstruktorius
+
+    // konstruktorius su parametrais
     Student(const std::string& firstName, const std::string& lastName, int examResults, const std::vector<int>& homeworkResults); 
 
+    // copy konstruktorius
     Student(const Student& other)
         : firstName(other.firstName), lastName(other.lastName),
           homeworkResults(other.homeworkResults), examResults(other.examResults) {}
 
-    Student& operator=(const Student& other) {
-        if (this != &other) {
-            firstName = other.firstName;
-            lastName = other.lastName;
+    Student& operator=(const Student& other) { // priskyrimo operatorius
+        if (this != &other) { // patikrinimas ar nera lygus pats sau 
+            firstName = other.firstName; //
+            lastName = other.lastName; 
             homeworkResults = other.homeworkResults;
             examResults = other.examResults;
         }
-        return *this;
+        return *this; // grazina objekta
     }
 
-    ~Student() {}
+    ~Student() {} // destruktorius
 
+    // geteriai ir seteriai 
     inline std::string getFirstName() const { return firstName; }
     inline std::string getLastName() const { return lastName; }
     inline std::vector<int> getHomeworkResults() const { return homeworkResults; }
@@ -49,6 +52,7 @@ public:
     void setExamResults(int examResults) { this->examResults = examResults; }
     void setHomeworkResults(std::vector<int> results) { homeworkResults = std::move(results); }
 
+    // funkcijos
     double calculateMedian() const;
     double calculateAverage() const;
     double calculateFinalGrade(bool median) const;
